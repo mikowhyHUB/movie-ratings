e6555dbacd9a16ea77d46c1ff1a367dc333cbb1d
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-11-26 16:59:54 +0100

    finished program

--- frontend.py
+++ frontend.py
@@ -1,31 +1,24 @@
 from tkinter import *
 import backend
-'''
-Dodać: nazwę, rezysera, rok produkcji, ocena, opcje, lista filmów
-
-Uzytkownik moze:
--prejrzeć wszystkie filmy
--wyszukać film
--segregowac filmy po ocenie
--segregować po roku produkcji
--updateowac inofmracje o filmie
--usuwać film
-'''
-# choosing index in listbox
 
 
 def get_selected_row(event):
-    global selected_tuple
-    index = list_films.curselection()[0]  # grabing index 0 of the tuple
-    selected_tuple = list_films.get(index)
-    e1.delete(0, END)
-    e1.insert(END, selected_tuple[1])
-    e2.delete(0, END)
-    e2.insert(END, selected_tuple[2])
-    e3.delete(0, END)
-    e3.insert(END, selected_tuple[3])
-    e4.delete(0, END)
-    e4.insert(END, selected_tuple[4])
+    # protecting program from index error
+    try:
+        global selected_tuple
+        index = list_films.curselection()[0]  # grabing index 0 of the tuple
+        selected_tuple = list_films.get(index)
+        # item shows in entry box after selecting(then you can update it)
+        e1.delete(0, END)
+        e1.insert(END, selected_tuple[1])
+        e2.delete(0, END)
+        e2.insert(END, selected_tuple[2])
+        e3.delete(0, END)
+        e3.insert(END, selected_tuple[3])
+        e4.delete(0, END)
+        e4.insert(END, selected_tuple[4])
+    except IndexError:
+        pass
 
 
 def view_command():
@@ -51,11 +44,17 @@ def add_command():
 
 def delete_command():
     backend.delete(selected_tuple[0])
+    # restarting list after deleting item
     list_films.delete(0, END)
 
 
-window = Tk()
+def update_command():
+    backend.update(selected_tuple[0], title_text.get(
+    ), year_text.get(), director_text.get(), rating_text.get())
 
+
+window = Tk()
+# assigning labels
 l1 = Label(window, text='Film')
 l1.grid(row=6, column=0)
 
@@ -67,7 +66,7 @@ l3.grid(row=6, column=2)
 
 l4 = Label(window, text='Rating')
 l4.grid(row=7, column=2)
-
+# assigining entry boxes
 title_text = StringVar()
 e1 = Entry(window, textvariable=title_text)
 e1.grid(row=6, column=1)
@@ -83,31 +82,31 @@ e3.grid(row=6, column=3)
 rating_text = StringVar()
 e4 = Entry(window, textvariable=rating_text)
 e4.grid(row=7, column=3)
-
+# assigning listbox
 list_films = Listbox(window, height=8, width=35)
 list_films.grid(row=0, column=0, columnspan=2, rowspan=6)
-
+# assigning scrollbar to list of films
 scrollbar = Scrollbar(window)
 scrollbar.grid(row=1, column=2)
-# assigning scrollbar to list of films
+
 list_films.configure(yscrollcommand=scrollbar.set)
 scrollbar.configure(command=list_films.yview)
 
 list_films.bind('<<ListboxSelect>>', get_selected_row)
-
+# assigining buttons
 b0 = Button(window, text='View all', width=12, command=view_command)
 b0.grid(row=0, column=3)
 
 b1 = Button(window, text='Search', width=12, command=search_command)
 b1.grid(row=1, column=3)
 
-b2 = Button(window, text='Sort', width=12)
-b2.grid(row=2, column=3)
+# b2 = Button(window, text='Sort', width=12)
+# b2.grid(row=2, column=3)
 
 b3 = Button(window, text='Delete', width=12, command=delete_command)
 b3.grid(row=3, column=3)
 
-b4 = Button(window, text='Update', width=12)
+b4 = Button(window, text='Update', width=12, command=update_command)
 b4.grid(row=4, column=3)
 
 b5 = Button(window, text='Add', width=12, command=add_command)
